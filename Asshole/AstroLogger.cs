9cde2c903da11b6357c6ecc8a0eb5ea3967b5116
moriumim <tanel.93@hotmail.com>
2017-03-29 19:51:52 +0300

    Take this asshole!

---
+++ Asshole/AstroLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightoxTools.Logging
+{
+    public class AstroLogger
+    {
+        public static IExceptionLogger ExceptionLogger;
+
+        public static void LogException(string exceptionSource, Exception exception)
+        {
+            ExceptionLogger?.Handle(exceptionSource, exception);
+        }
+
+        public static void LogText(string text, bool important = false)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
